92150edb205d85c4ff8a42d27c7ac58465a225a1
Sreekanth Govindaiah <31440235+sreekanth-bg@users.noreply.github.com>
2019-05-18 22:22:58 +0530

    create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+sudo su
+
+# Update hosts file
+echo "[TASK 1] Update /etc/hosts file"
+cat >>/etc/hosts<<EOF
+172.42.42.100 kmaster.example.com kmaster
+172.42.42.101 kworker1.example.com kworker1
+172.42.42.102 kworker2.example.com kworker2
+EOF
+
+# Install docker from Docker-ce repository
+echo "[TASK 2] Install docker container engine"
+sudo apt-get update
+sudo apt-get install -y docker.io
+
+# Enable docker service
+echo "[TASK 3] Enable and start docker service"
+sudo service docker start
+
+# Disable swap
+echo "[TASK 7] Disable and turn off SWAP"
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+# Add yum repo file for Kubernetes
+apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+
+# Install Kubernetes
+echo "[TASK 9] Install Kubernetes (kubeadm, kubelet and kubectl)"
+apt-get install -y kubelet kubeadm kubectl 
+
+# Start and Enable kubelet service
+echo "[TASK 10] Enable and start kubelet service"
+systemctl enable kubelet >/dev/null 2>&1
+systemctl start kubelet >/dev/null 2>&1
+
+# Enable ssh password authentication
+echo "[TASK 11] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+systemctl reload sshd
+
+# Set Root password
+echo "[TASK 12] Set root password"
+echo "kubeadmin" | passwd --stdin root >/dev/null 2>&1
+
+# Update vagrant user's bashrc file
+echo "export TERM=xterm" >> /etc/bashrc
