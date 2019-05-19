401e43233aabcb9a621fa0753523fbb11740a5a7
Sreekanth Govindaiah <31440235+sreekanth-bg@users.noreply.github.com>
2019-05-19 09:17:37 +0530

    Update bootstrap_kworker.sh

--- bootstrap_kworker.sh
+++ bootstrap_kworker.sh
@@ -2,6 +2,7 @@
 
 # Join worker nodes to the Kubernetes cluster
 echo "[TASK 1] Join node to Kubernetes Cluster"
-sudo apt-get install sshpass >/dev/null 2>&1
-sshpass -p "vagrant" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vagrant@kmaster.example.com:/joincluster.sh /joincluster.sh 2>/dev/null
-bash /joincluster.sh >/dev/null 2>&1
+#sudo apt-get install sshpass >/dev/null 2>&1
+#sshpass -p "vagrant" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vagrant@kmaster.example.com:/joincluster.sh /joincluster.sh 2>/dev/null
+#bash /joincluster.sh >/dev/null 2>&1
+bash hostname
